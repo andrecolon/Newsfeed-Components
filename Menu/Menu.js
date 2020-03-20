829b7312464c75a6a3e9fd02be0a0abb78b022f2
Andre <andrecolon@gmail.com>
2020-03-19 18:25:07 -0700

    Added all mvp

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,12 @@
 /* This is the data we will be using, study it but don't change anything, yet. */
 
 let menuItems = [
-  'Students',
-  'Faculty',
-  "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+    'Students',
+    'Faculty',
+    "What's New",
+    'Tech Trends',
+    'Music',
+    'Log Out'
 ];
 
 /* 
@@ -21,6 +21,8 @@ let menuItems = [
 
   The function takes an array as its only argument.
 
+
+
   Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
   Add those items to the <ul>
 
@@ -33,3 +35,44 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+
+
+let createMenu = menuArray => {
+    //  Step 1: Write a function that will create a menu component as seen below:
+
+    //Create elements
+    let menu = document.createElement("div");
+    let menuList = document.createElement("ul");
+
+    //append nodes
+    menu.appendChild(menuList);
+
+    //Add classes
+    menu.classList.add("menu");
+
+    console.log(menu);
+
+    //
+    menuArray.forEach(el => {
+        let menuItem = document.createElement("li");
+        menuItem.textContent = el;
+        menuList.appendChild(menuItem);
+    });
+
+    let menuButton = document.querySelector(".menu-button");
+    //  Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
+
+
+    menuButton.addEventListener("click", () => {
+        //  Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
+
+        menu.classList.toggle("menu--open");
+    });
+
+    return menu;
+    // Step 5: return the menu component.
+};
+
+let header = document.querySelector(".header");
+// / Step 6: add the menu component to the DOM.
+header.appendChild(createMenu(menuItems));
